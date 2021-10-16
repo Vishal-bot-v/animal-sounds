53460d513d70ce63ac8904fd9067e276876e8df9
Vishal-bot-v <79948201+Vishal-bot-v@users.noreply.github.com>
2021-10-15 20:16:47 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+function startClassification()
+{
+  navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/c3RXMAjBO/json.model', modelReady);
+}
+
+function modelReady(){
+  classifier.classify( gotResults);
+}
+
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    random_number_r = Math.floor(Math.random() * 255) + 1;
+    random_number_g = Math.floor(Math.random() * 255) + 1;
+    random_number_b = Math.floor(Math.random() * 255) + 1;
+
+    document.getElementById("result_label").innerHTML = 'I can hear - '+ results[0].label;
+    document.getElementById("result_confidence").innerHTML = 'Accuracy - '+ (results[0].confidence*100).toFixed(2)+" %";
+    document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+    document.getElementById("result_confidence").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+
+    img = document.getElementById('alien1') 
+    img1 = document.getElementById('alien2')
+    img2 = document.getElementById('alien3')
+    img3 = document.getElementById('alien4')
+
+    if (results[0].label == "bark") {
+      img.src = 'dog-dog.gif';
+      img1.src = 'aliens-02.png';
+      img2.src = 'aliens-03.png';
+      img3.src = 'aliens-04.png';
+    } else if (results[0].label == "meow") {
+      img.src = 'aliens-01.png';
+      img1.src = 'cat-meow.gif';
+      img2.src = 'aliens-03.png';
+      img3.src = 'aliens-04.png';
+    } else if (results[0].label == "tweet") {
+      img.src = 'aliens-01.png';
+      img1.src = 'aliens-02.png';
+      img2.src = 'bird-blue.gif';
+      img3.src = 'aliens-04.png';
+    }else{
+      img.src = 'aliens-01.png';
+      img1.src = 'aliens-02.png';
+      img2.src = 'aliens-03.png';
+      img3.src = 'lion-roar.gif';
+    }
+  }
+}
